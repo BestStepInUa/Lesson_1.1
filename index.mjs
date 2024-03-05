dcd37a803c0f82d21aa532a6862f6f8ea41ee5f8
BestStepInUa <beststep.in.ua@gmail.com>
2024-03-05 17:13:17 +0200

    fixed

--- index.mjs
+++ index.mjs
@@ -2,15 +2,14 @@
 // const nodemon = require('nodemon');
 // const { admins, clients } = require('./node-CommonJS-module/users');
 // ES6 mjs
-import { admins, clients } from './node-CommonJS-module/users.mjs';
+import users from './node-module-es6-mjs/users.mjs';
 
 console.log(`Hello from Node.js!`);
-console.log(admins);
-console.log(clients);
+console.log(users);
 
 // CommonJS
 // const { getCurrentMonth } = require('./node-CommonJS-module/data');
 // ES6 mjs
-import { getCurrentMonth } from './node-CommonJS-module/data/index.mjs';
+import { getCurrentMonth } from './node-module-es6-mjs/data/index.mjs';
 const currentMonth = getCurrentMonth();
 console.log(`Now ${currentMonth} month`);
